85538b07d0cb850fe61994c3ab4d315c33228c43
Michelle Bentivegna <mcbentivegna@gmail.com>
2020-05-16 22:18:45 -0400

    Updates

---
+++ precache-manifest.964b73ae2d3401eda67777a22dc8c5c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1618b720c7be6cac46967c0752e51a80",
+    "url": "/course-directory/index.html"
+  },
+  {
+    "revision": "43a2fe7287cf077c6ac9",
+    "url": "/course-directory/static/css/main.bbb482cf.chunk.css"
+  },
+  {
+    "revision": "d847397b2a19011045ad",
+    "url": "/course-directory/static/js/2.04b8f18b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/course-directory/static/js/2.04b8f18b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43a2fe7287cf077c6ac9",
+    "url": "/course-directory/static/js/main.135ecea9.chunk.js"
+  },
+  {
+    "revision": "07240a532d920a52483f",
+    "url": "/course-directory/static/js/runtime-main.4157ec61.js"
+  }
+]);
